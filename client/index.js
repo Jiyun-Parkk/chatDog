019b724744678cabfac728422cb118a9c24d1004
박지윤 <jyun1937@naver.com>
2023-03-26 14:03:06 +0900

    Chore: chatdog setup

---
+++ client/index.js
@@ -0,0 +1,72 @@
+const chatWindow = document.querySelector('.chat-window');
+const messageBox = document.querySelector('.chat-messages');
+const messageList = document.querySelector('.chat-messages ul');
+const chatForm = document.querySelector('#chat');
+const dateForm = document.querySelector('#date-form');
+const inputBox = document.querySelector('.chat-input input[type="text"]');
+const sendButton = document.querySelector('.chat-input button');
+const spinner = document.querySelector('#loader');
+let userMessages = [];
+let assistantMessages = [];
+let myDateTime = '';
+
+chatForm.addEventListener('submit', sendMessage);
+dateForm.addEventListener('submit', sendDate);
+
+async function sendDate(e) {
+  e.preventDefault();
+  let formData = new FormData(dateForm);
+  const date = formData.get('date');
+  const time = formData.get('time');
+
+  if (!date || date === '') {
+    alert('생년월일을 입력해주세요');
+  } else if (!time || time === '') {
+    alert('태어난 시간을 입력해주세요');
+  }
+  myDateTime = `생년월일:${date},태어난시:${time.split(':')[0]}`;
+
+  dateForm.style.display = 'none';
+  chatForm.style.display = 'flex';
+}
+async function sendMessage(e) {
+  e.preventDefault();
+
+  let messageText = inputBox.value;
+  const newMessage = document.createElement('li');
+  newMessage.textContent = messageText;
+  messageList.appendChild(newMessage);
+  userMessages.push(messageText);
+  inputBox.value = '';
+
+  try {
+    spinner.style.display = 'flex';
+    sendButton.disabled = true;
+    const response = await fetch('http://localhost:8080/fortuneTell', {
+      method: 'POST',
+      body: JSON.stringify({
+        myDateTime,
+        userMessages,
+        assistantMessages,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    assistantMessages.push(data.assistant);
+    const newResponse = document.createElement('li');
+    newResponse.textContent = data.assistant;
+    newResponse.classList.add('answer');
+    spinner.style.display = 'none';
+    messageList.appendChild(newResponse);
+    sendButton.disabled = false;
+  } catch (error) {
+    console.error(error);
+  }
+}
