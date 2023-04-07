176a3d83f34f2a652092f9a2869d33bb96f2831f
박지윤 <jyun1937@naver.com>
2023-04-07 21:35:33 +0900

    Style: header fixed style 변경

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
-const CustomHeader = styled.header`
+const CustomHeader = styled.header<{ route: string }>`
+  display: ${({ route }) => (route.includes('chat') ? 'none' : 'block')};
   border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 20;
+  background: rgba(255, 255, 255, 0.2);
   div {
     padding: 10px;
     display: flex;
@@ -15,20 +23,22 @@ const CustomHeader = styled.header`
       white-space: nowrap;
       font-size: 1.1rem;
     }
-    div {
-      width: 40px;
-      height: 40px;
-      background: url('static/images/profile.png') no-repeat center / cover, skyblue;
+    img {
+      background: skyblue;
       border-radius: 20px;
     }
   }
 `;
 
 export const Header = () => {
+  const router = useRouter();
+  const handleClickLogo = () => {
+    router.push('/');
+  };
   return (
-    <CustomHeader>
-      <div>
-        <div />
+    <CustomHeader route={router.pathname}>
+      <div onClick={handleClickLogo}>
+        <Image src="/static/images/profile.png" width={40} height={40} alt="logo" />
         <h1>챗독</h1>
       </div>
     </CustomHeader>
