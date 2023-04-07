872cdd1d530779ad0773929e75f73fee7d447cb7
박지윤 <jyun1937@naver.com>
2023-04-07 22:27:50 +0900

    Feat: chatdog info 상수로 추가

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Button } from '@mui/material';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
-import { CHAT } from '@/consts/chatType';
+import { chatDogList } from '@/consts/chatDogInfo';
+import { Title } from '@/components';
 
 const Container = styled.section`
   padding-top: 60px;
@@ -21,21 +22,6 @@ const Container = styled.section`
       flex-direction: column;
       justify-content: space-between;
       gap: 20px;
-      h2 {
-        width: fit-content;
-        font-size: 1.8rem;
-        position: relative;
-        &:before {
-          content: '';
-          width: 100%;
-          height: 15px;
-          position: absolute;
-          bottom: 3px;
-          left: 10px;
-          z-index: -1;
-          background: rgba(182, 46, 63, 0.4);
-        }
-      }
       img {
         position: static !important;
         border-radius: 20px;
@@ -85,7 +71,7 @@ export default function Home() {
         <ul>
           {chatDogList.map((list) => (
             <li key={list.title}>
-              <h2>{list.title}</h2>
+              <Title titleText={list.title} subtext={list.sub} />
               <Image src={list.imgPath} fill alt={list.title} priority />
               <article>
                 <div className="tagbox">
@@ -94,8 +80,8 @@ export default function Home() {
                   ))}
                 </div>
                 <p>{list.explain}</p>
-                <Button onClick={() => handleClickBanner(list.path)}>
-                  {list.go} 물어보러 가기 <BsFillArrowRightCircleFill />
+                <Button onClick={() => handleClickBanner(`/detail/${list.title}`)}>
+                  {list.keyword} 물어보러 가기 <BsFillArrowRightCircleFill />
                 </Button>
               </article>
             </li>
@@ -105,30 +91,3 @@ export default function Home() {
     </Container>
   );
 }
-
-const chatDogList = [
-  {
-    path: `/detail/${CHAT.FORTUNE}`,
-    title: '포춘독',
-    imgPath: '/static/images/fortune.png',
-    tag: ['#운세', '#사주', '#취업운', '#연애운', '#건강운', '#시험운', '#토정비결', '#신년운세'],
-    explain: '귀여운 포춘독이 당신의 운세를 알려드립니다',
-    go: '운세',
-  },
-  {
-    path: `/detail/${CHAT.RECIPE}`,
-    title: '레시피독',
-    imgPath: '/static/images/fortune.png',
-    tag: ['#레시피', '#음식', '#요리', '#오늘 뭐먹지?'],
-    explain: '레시피독은 이 세상 모든 요리법을 알고 있어요',
-    go: '레시피',
-  },
-  {
-    path: `/detail/${CHAT.DIET}`,
-    title: '다이어트독',
-    imgPath: '/static/images/fortune.png',
-    tag: ['#다이어트', '#음식', '#요리', '#식단관리', '#체중감량'],
-    explain: '다이어트독으로 당신의 식습관을 바꿔보세요',
-    go: '식단',
-  },
-];
