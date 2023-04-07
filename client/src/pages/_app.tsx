00bccd002b32d947761f343cba93c557828aa6da
박지윤 <jyun1937@naver.com>
2023-04-07 23:22:10 +0900

    Feat: Recoil 초기 세팅

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,19 +2,22 @@ import { Layout } from '@/components';
 import { GlobalStyle } from '@/styles/globalstyle';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
       <Script
-        src='https://www.googletagmanager.com/gtag/js?id=G-QDHX3HN0PZ'
-        strategy='afterInteractive'
+        src="https://www.googletagmanager.com/gtag/js?id=G-QDHX3HN0PZ"
+        strategy="afterInteractive"
       />
-      <Script id='google-analytics' strategy='afterInteractive'>
+      <Script id="google-analytics" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];
       function gtag() {
         dataLayer.push(arguments);
