22aeafa3b1311a46f33863a14da8cbdc4648517a
박지윤 <jyun1937@naver.com>
2023-04-11 14:32:23 +0900

    Feat: viewport 세팅 변경

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -10,7 +10,10 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
+        />
       </Head>
       <Header />
       <main>{children}</main>
