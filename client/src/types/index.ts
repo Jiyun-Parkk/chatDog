0bf968d16432dfbc29c94d011941e9932e68e38e
박지윤 <jyun1937@naver.com>
2023-04-11 11:20:50 +0900

    Feat: type 추가 및 수정

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,4 +1,11 @@
 export interface FortuneFormType {
+  [key: string]: string;
+  Year: string;
+  Month: string;
+  Day: string;
+  Time: string;
+}
+export interface FortuneBirthType {
   date: string;
   time: string;
 }
