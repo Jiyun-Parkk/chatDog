ea65e013f0daa263ceb662019734abc118e9bf89
박지윤 <jyun1937@naver.com>
2023-04-15 23:24:45 +0900

    Style: header 스타일 변경

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,52 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
-import Head from 'next/head';
-import profileImage from '/public/static/images/profile.png';
+import { MdChevronLeft } from 'react-icons/md';
+import { Button } from '@mui/material';
 
 const CustomHeader = styled.header<{ route: string }>`
-  display: ${({ route }) => (route.includes('chat') ? 'none' : 'block')};
-  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-  position: fixed;
-  top: 0;
-  left: 0;
   width: 100%;
   z-index: 20;
   background: rgba(255, 255, 255, 0.5);
-  div {
-    padding: 10px;
+  .back-btn {
+    color: #000;
+    aspect-ratio: 1 / 1;
+    font-size: 32px;
     display: flex;
+    margin-left: -16px;
     align-items: center;
-    gap: 5px;
+    justify-content: flex-start;
     cursor: pointer;
-
-    h1 {
-      white-space: nowrap;
-      font-size: 1.1rem;
-    }
-    img {
-      background: skyblue;
-      border-radius: 20px;
-    }
   }
 `;
 
 export const Header = () => {
   const router = useRouter();
-  const handleClickLogo = () => {
-    router.push('/');
+  const handleClickBack = () => {
+    router.back();
   };
   return (
     <CustomHeader route={router.pathname}>
-      <Head>
-        <title>AI 강아지 챗독</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div onClick={handleClickLogo}>
-        <Image src={profileImage} width={40} height={40} alt="logo" priority />
-        <h1>챗독</h1>
-      </div>
+      <Button className="back-btn" onClick={handleClickBack}>
+        <MdChevronLeft />
+      </Button>
     </CustomHeader>
   );
 };
