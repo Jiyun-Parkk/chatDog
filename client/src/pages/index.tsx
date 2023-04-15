0bd7f61d574e823c00ec5e7104a725807e18e3b1
박지윤 <jyun1937@naver.com>
2023-04-15 23:27:00 +0900

    Style: 메인 UI 수정

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,67 +3,87 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { Button } from '@mui/material';
 import { chatDogList } from '@/consts/chatDogInfo';
-import { KakaoAdFit, Title } from '@/components';
-import { MdChevronRight } from 'react-icons/md';
+import { KakaoAdFit } from '@/components';
 
 const Container = styled.section`
-  padding-top: 60px;
   ul {
     margin: 0 auto;
     width: 100%;
+    height: 100vh;
     display: flex;
-    gap: 20px;
-    padding: 30px;
-    /*justify-content: space-between;*/
-    justify-content: center;
-    li {
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      gap: 20px;
-      img {
-        position: static !important;
-        border-radius: 20px;
-        aspect-ratio: 1 / 1;
-        cursor: pointer;
-      }
-      .tagbox {
-        padding: 10px 0;
-        height: 60px;
-        span {
-          color: gray;
-          font-size: 0.9rem;
-        }
-      }
-      button {
-        align-self: flex-start;
-        background: rgba(182, 46, 63);
-        color: #fff;
-        display: flex;
-        align-items: center;
-        gap: 5px;
-        font-size: 1.1rem;
-        padding: 10px;
-        margin-top: 20px;
-        &:hover {
-          background: rgba(182, 46, 65, 0.9);
-        }
-      }
-    }
+    justify-content: space-between;
   }
   @media (max-width: 1024px) {
     ul {
-      width: 80%;
-      gap: 50px;
+      height: auto;
       flex-direction: column;
     }
   }
-  @media (max-width: 750px) {
-    ul {
-      width: 100%;
-      gap: 50px;
-      flex-direction: column;
+`;
+
+const DogContentList = styled.li<{ color: string }>`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  gap: 20px;
+  padding: 30px 0;
+  background: ${({ color }) => color};
+  p {
+    color: #fff;
+    padding-left: 24px;
+  }
+  .main-text {
+    height: fit-content;
+    p {
+      font-size: 28px;
+      font-weight: 600;
+    }
+    .tagbox {
+      width: fit-content;
+      padding-left: 24px;
+      height: 60px;
+      display: flex;
+      gap: 5px;
+      align-items: center;
+      flex-wrap: wrap;
+      width: 70%;
+      span {
+        background: rgba(255, 255, 255, 0.2);
+        border-radius: 40px;
+        padding: 5px;
+        color: #fff;
+        font-size: 0.8rem;
+      }
+    }
+  }
+  img {
+    width: 100%;
+    height: calc(100vw / 4);
+    aspect-ratio: 1 / 1;
+    cursor: pointer;
+  }
+
+  button {
+    width: 90%;
+    margin: 0 auto;
+    border: 2px solid transparent;
+    background: rgba(255, 255, 255, 1);
+    color: #000;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    padding: 16px;
+    cursor: pointer;
+    &:hover {
+      border: 2px solid rgba(255, 255, 255, 0.5);
+      color: #fff;
+    }
+  }
+  @media (max-width: 1024px) {
+    img {
+      height: auto;
     }
   }
 `;
@@ -75,38 +95,39 @@ export default function Home() {
   };
   return (
     <Container>
-      <KakaoAdFit id="DAN-uN1thLqMAEjPbvRl" />
       <section>
         <ul>
           {chatDogList.map((list) => (
-            <li key={list.title}>
-              <Title titleText={list.title} />
-              <Image
-                src={list.imgPath}
-                alt={list.title}
-                sizes="100"
-                fill
-                priority
-                placeholder="blur"
-                onClick={() => handleClickBanner(`/detail/${list.title}`)}
-              />
-              <article>
+            <DogContentList key={list.title} color={list.color.point}>
+              <p>{list.sub}</p>
+              <div className="main-text">
+                {list.mainText.map((text) => (
+                  <p>{text}</p>
+                ))}
                 <div className="tagbox">
                   {list.tag.map((tag, idx) => (
                     <span key={idx}>{tag} </span>
                   ))}
                 </div>
-                <p>{list.explain}</p>
+              </div>
+              <Image
+                src={list.imgPath}
+                alt={list.title}
+                priority
+                placeholder="blur"
+                onClick={() => handleClickBanner(`/detail/${list.title}`)}
+              />
+              <article>
                 <Button onClick={() => handleClickBanner(`/detail/${list.title}`)}>
-                  {list.keyword} 물어보러 가기
-                  <MdChevronRight />
+                  {list.keyword} 시작하기
                 </Button>
               </article>
-            </li>
+            </DogContentList>
           ))}
         </ul>
       </section>
       <KakaoAdFit id="DAN-iHu4K4WXJEEUcnyr" />
+      <KakaoAdFit id="DAN-uN1thLqMAEjPbvRl" />
     </Container>
   );
 }
